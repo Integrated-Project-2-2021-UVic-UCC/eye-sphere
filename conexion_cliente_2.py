14c50615413d01723bfb85e78bcf72d4e5a1436a
SergiMuac <72520395+SergiMuac@users.noreply.github.com>
2021-02-22 23:08:03 +0100

    Streaming without compression

--- conexion_cliente_2.py
+++ conexion_cliente_2.py
@@ -13,22 +13,12 @@ data = b""
 payload_size = struct.calcsize("Q")
 msg_size = 640*480*3
 while True:
-##    while len(data) < payload_size:
-##        packet = miSocket.recv(4*1024)
-##        if not packet:
-##            break
-##        data+=packet
-##    packed_msg_size=data[:payload_size]
-##    data=data[payload_size:]
-##    msg_size=struct.unpack("Q",packed_msg_size)[0]
-##
+    
     while len(data) < msg_size:
         data+=miSocket.recv(msg_size)
         
     frame_data=data[:msg_size]
     data=data[msg_size:]
-
-    #frame_data = miSocket.recv(640*480*3)
 
     frame = np.frombuffer(frame_data,np.uint8)
     frame = frame.reshape((480, 640, 3))
@@ -40,31 +30,3 @@ while True:
         break
 miSocket.close()
 
-    
-"""
-    ret, frame=cap.read()
-    #cv2.imshow('VisorOriginal',frame)
-    miSocket=socket.socket() #creamos objeto socket
-    #data=input("mensaje: ")
-    msg=pickle.dumps(frame)#bytes(data,'utf-8')
-    miSocket.send(msg)
-    respuesta = miSocket.recv(1024)#1024 hace referencia la bufer, es decir 1024 bytes
-    #respuesta=list(str(respuesta,'ascii'))
-    print(respuesta)
-    miSocket.close()
-        
-    #si pulsamos q finalizamos el bucle
-    if cv2.waitKey(1) and (0xFF==ord('q')):
-        break
-"""
-
-"""
-cap.release()
-cv2.destroyAllWindows()
-    msg=bytes(data,'utf-8')
-    miSocket.send(msg)
-    respuesta = miSocket.recv(1024)#1024 hace referencia la bufer, es decir 1024 bytes
-    #respuesta=list(str(respuesta,'ascii'))
-    print(respuesta)
-    miSocket.close()
-"""
