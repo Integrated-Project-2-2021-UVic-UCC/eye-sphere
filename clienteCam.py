33d1b305faf42bae8ae315f35b7b3f9f349caffd
Sergi <sergimuac@gmail.com>
2021-03-08 18:05:10 +0100

    Commented new bad lines
    As the code is been modified, all new lines that make the code stop are commented

--- clienteCam.py
+++ clienteCam.py
@@ -10,10 +10,18 @@ class Cliente():
         def __init__(self, host="servermuac.ddns.net", port=21):
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.sock.connect((str(host), int(port)))
-                #self.sock.send(pickle.dumps("cam1"))
+                #self.name="cam1"
+                #self.sock.sendall(bytes(f"{self.name:6}","utf-8"))
                 self.video=cv2.VideoCapture(0)
                 msg_recv = threading.Thread(target=self.msg_recv)
                 stream=threading.Thread(target=self.stream)
+
+                
+##                while True:
+##                        resp=self.sock.recv(2*1024)
+##                        if resp:
+##                                print(str(resp,'ascii').strip())
+##                                break
 
                 msg_recv.daemon = True
                 msg_recv.start()
@@ -32,7 +40,7 @@ class Cliente():
                 while True:
                         frame=self.video.read()[1]
                         cv2.imshow('videoCam',frame)
-                        buf=cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,20])[1].tobytes()#imencode devuelve 2 valores, pero nos quedamos con el 2nd que es lo que nos interesa
+                        buf=cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,10])[1].tobytes()#imencode devuelve 2 valores, pero nos quedamos con el 2nd que es lo que nos interesa
                         header = bytes(f"{len(buf):6.0f}",'utf-8')
                         self.packet=header+buf
                         self.sock.sendall(self.packet)
